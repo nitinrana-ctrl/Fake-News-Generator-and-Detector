9e2b0aa7fcfad641585cdca63aa39dbf75c1a159
nitinrana-ctrl <nitin.rana.2k.18@gmail.com>
2026-07-25 20:04:10 +0530

    Support Streamlit secrets

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
-    raise ValueError("GEMINI_API_KEY not found in .env file")
+    api_key = st.secrets.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
